fa42b870a0d5d7f910b2d64ab9034b267e09e984
siddharth91520 <sidharth@retire100.live.com>
2024-10-30 11:09:33 +0530

    fixed vercel deployment issue

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h1 className='text-5xl text-slate-600 font-bold'>Contact page</h1>
+    </div>
+  )
+}
+
+export default Contact
